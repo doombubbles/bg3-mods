0c096130fb12e92831904d2a10015e40b39f994a
doombubbles <obsidiantoolzg@gmail.com>
2024-07-09 14:32:34 -0700

    Refactor values.yaml

--- ProcessTemplates/ProcessTemplates.go
+++ ProcessTemplates/ProcessTemplates.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -13,40 +14,41 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-type Values map[string]interface{}
-
-var excludedExtensions = []string{
+var nonTemplateExtensions = []string{
 	".dds",
+}
+var skipExtensions = []string{
+	".yaml",
 }
 
 const buildDir = "build"
 
+type Values map[string]interface{}
+type AnyValue interface{}
+
 func main() {
-	// Mod folder name should be the same as root dir
-	var rootDir, err = os.Getwd()
+	// Get the current directory
+	rootDir, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("error getting current directory: %v", err)
 	}
-	rootDir = filepath.Base(rootDir)
+
+	modDir := filepath.Base(rootDir)
 
 	// Delete all files in the build directory
-	err = cleanBuildDirectory(filepath.Join(buildDir, rootDir))
+	err = cleanBuildDirectory(filepath.Join(buildDir, modDir))
 	if err != nil {
 		log.Fatalf("error cleaning build directory: %v", err)
 	}
 
-	// Read the values from values.yaml
-	var values Values = nil
-	valuesData, err := os.ReadFile("values.yaml")
-	if err == nil {
-		err = yaml.Unmarshal(valuesData, &values)
-		if err != nil {
-			log.Fatalf("error parsing values.yaml: %v", err)
-		}
+	// Build the values map
+	values, err := buildValuesMap(modDir)
+	if err != nil {
+		log.Fatalf("error building values map: %v", err)
 	}
 
 	// Walk through and process all files
-	err = filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+	err = filepath.Walk(modDir, func(path string, info os.FileInfo, err error) error {
 		return processFile(path, info, err, values)
 	})
 	if err != nil {
@@ -68,6 +70,79 @@ func cleanBuildDirectory(path string) error {
 
 	log.Println("Build directory cleaned")
 	return nil
+}
+
+func buildValuesMap(rootDir string) (Values, error) {
+	values := make(Values)
+
+	err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Ignore buildDir and directories starting with "."
+		if info.IsDir() {
+			if info.Name() == buildDir || strings.HasPrefix(info.Name(), ".") {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+
+		// Check if the file is a .yaml file
+		if strings.HasSuffix(info.Name(), ".yaml") {
+			// Read the YAML file
+			data, err := os.ReadFile(path)
+			if err != nil {
+				return fmt.Errorf("error reading file %s: %v", path, err)
+			}
+
+			// Parse the YAML content
+			var fileValue AnyValue
+			err = yaml.Unmarshal(data, &fileValue)
+			if err != nil {
+				return fmt.Errorf("error parsing YAML in file %s: %v", path, err)
+			}
+
+			// Get the file name without extension
+			fileName := strings.TrimSuffix(info.Name(), filepath.Ext(info.Name()))
+
+			// Add the parsed values to the main values map
+			parts := strings.Split(fileName, ".")
+			current := values
+			for i, part := range parts {
+				if i == len(parts)-1 {
+					// Last part, assign the actual value
+					current[part] = fileValue
+				} else {
+					// Create nested map if it doesn't exist
+					if _, exists := current[part]; !exists {
+						current[part] = make(Values)
+					}
+
+					if currentPart, ok := current[part].(Values); ok {
+						current = currentPart
+					} else {
+						err = &yaml.TypeError{}
+					}
+				}
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error processing YAML files: %v", err)
+	}
+
+	yamlData, err := yaml.Marshal(values)
+	if err != nil {
+		fmt.Printf("  Error marshaling YAML: %v\n", err)
+	} else {
+		fmt.Printf("%s\n", string(yamlData))
+	}
+
+	return values, nil
 }
 
 func include(tmpl *template.Template) func(name string, data interface{}) (string, error) {
@@ -102,6 +177,10 @@ func processFile(path string, info os.FileInfo, err error, values Values) error 
 	}
 
 	outputPath := filepath.Join(outputDir, filepath.Base(path))
+
+	if isSkipped(path) {
+		return nil
+	}
 
 	// Check if the file should be excluded from template processing
 	if isExcluded(path) {
@@ -149,7 +228,17 @@ func processFile(path string, info os.FileInfo, err error, values Values) error 
 
 func isExcluded(path string) bool {
 	lowercasePath := strings.ToLower(path)
-	for _, ext := range excludedExtensions {
+	for _, ext := range nonTemplateExtensions {
+		if strings.HasSuffix(lowercasePath, strings.ToLower(ext)) {
+			return true
+		}
+	}
+	return false
+}
+
+func isSkipped(path string) bool {
+	lowercasePath := strings.ToLower(path)
+	for _, ext := range skipExtensions {
 		if strings.HasSuffix(lowercasePath, strings.ToLower(ext)) {
 			return true
 		}
